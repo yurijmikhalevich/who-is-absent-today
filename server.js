46e0bc9c07ee03f88a0bfaefc89c727e30fa5b42
Yurij Mikhalevich <0@39.yt>
2013-05-01 15:05:07 +0400

    Written students-related request handlers.

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
 var express = require('express')
+    , path = require('path')
+    , MongoClient = require('mongodb').MongoClient
     , app = express()
-    , path = require('path');
+    , db;
 
+MongoClient.connect('mongodb://localhost:27017/wiat', function (err, conn) {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        db = conn;
+    }
+);
 
 app.use(express.static(path.join(__dirname, 'client')));
 
-app.get('/', function(req, res) {
-    res.send('Hello, World!');
+app.post('/students/insert/', function(req, res) {
+    db.collection('students').insert({'name' : req.query.name}, {'safe' : true}, function(err, items) {
+        res.send(items[0]);
+    });
+});
+
+app.post('/students/remove/', function(req, res) {
+    db.collection('students').findAndRemove({'_id' : req.query.id}, [['_id', 'asc']], function(err, items) {
+        res.send(items[0]);
+    });
+});
+
+app.post('/students/update/', function(req, res) {
+    db.collection('students').update({'_id' : req.query.id}, {'name' : req.query.name}, function(err, count){
+        res.send(count);
+    });
+});
+
+app.get('/students/', function(req, res) {
+    db.collection('students').find().toArray(function(err, items) {
+        res.send(items);
+    });
 });
 
 app.listen(3000);
