cf817e56c013df3c072ef5b480a850f1d45f7994
Yurij Mikhalevich <0@39.yt>
2013-05-07 12:06:26 +0400

    zbs

--- server.js
+++ server.js
@@ -6,65 +6,68 @@ var express = require('express')
   , db;
 
 MongoClient.connect('mongodb://localhost:27017/wiat', function (err, conn) {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
-        db = conn;
+    if (err) {
+      console.log(err);
+      throw err;
     }
+    db = conn;
+  }
 );
 
+app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'client')));
 
 app.post('/students/insert/', function(req, res) {
-    db.collection('students').insert({'name' : req.body.name}, {'safe' : true}, function(err, items) {
-        res.send(items[0]);
-    });
+  db.collection('students').insert({ name : req.body.name }, { safe : true }, function(err, items) {
+    res.send(items[0]);
+  });
 });
 
 app.post('/students/remove/', function(req, res) {
-    db.collection('students').findAndRemove({'_id' : new ObjectId(req.body.id)}, [['_id', 'asc']],
-        {'safe' : true},
-        function(err, item) {
-            res.send(item);
-        }
-    );
+  db.collection('students').findAndRemove({ _id : new ObjectId(req.body.id) }, [['_id', 'asc']], { safe : true },
+    function(err, item) {
+      res.send(item);
+    }
+  );
 });
 
 app.post('/students/update/', function(req, res) {
-    db.collection('students').update({'_id' : new ObjectId(req.body.id)}, {'name' : req.query.name},
-        {'safe' : true},
-        function(err, count) {
-            res.send(count.toString());
-        }
-    );
+  db.collection('students').update({ _id : new ObjectId(req.body.id) }, { name : req.query.name },
+    { safe : true },
+    function(err, count) {
+      res.send(count.toString());
+    }
+  );
 });
 
 app.get('/students/', function(req, res) {
-    db.collection('students').find().toArray(function(err, items) {
-        res.send(items);
-    });
+  db.collection('students').find().toArray(function(err, items) {
+    res.send(items);
+  });
 });
 
 app.post('/attendance/save/', function(req, res) {
-    var attendance = {};
-    if (req.query.date) {
-        attendance.date = req.body.date;
-    }
-    attendance.present = [];
-    var splitted_present = req.body.present.split(',');
-    for (var i = 0; i < splitted_present.length; ++i) {
-        attendance.present.push(new ObjectId(splitted_present[i]));
-    }
-    db.collection('attendance').save(attendance, {'safe' : true}, function(err, items) {
-        res.send(items.toString());
-    });
+  var attendance = {};
+  if (req.body.date) {
+    attendance._id = req.body.date;
+  } else {
+    attendance._id = new Date().toISOString().substring(0, 10);
+  }
+  attendance.present = [];
+  var splitted_present = req.body.present.split(',');
+  for (var i = 0; i < splitted_present.length; ++i) {
+    attendance.present.push(new ObjectId(splitted_present[i]));
+  }
+  db.collection('attendance').save(attendance, { safe : true }, function(err, items) {
+    res.send(items.toString());
+  });
 });
 
 app.get('/attendance/', function(req, res) {
-    db.collection('attendance').find({'date' : req.query.date}).toArray(function(err, items) {
-        res.send(items[0]);
-    });
+  db.collection('attendance').findOne(
+      { _id : req.query.date || new Date().toISOString().substring(0, 10) }).toArray(function(err, item) {
+    res.send(item);
+  });
 });
 
-app.listen(3000);
+app.listen(8000);
